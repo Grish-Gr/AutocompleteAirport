87a309c1cc6ff483e37d39789ebbf2dab83efd61
Grish-Gr <101037462+Grish-Gr@users.noreply.github.com>
2023-05-04 09:59:47 +0500

    init

---
+++ src/main/java/maksim/ter/Main.java
@@ -0,0 +1,21 @@
+package maksim.ter;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Date;
+import java.util.List;
+import java.util.SortedMap;
+import java.util.TreeMap;
+import java.util.stream.Stream;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
